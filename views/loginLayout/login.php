d882b80593ed41d24676a2b90960054caabfb6f5
unknown <karimrehmat942@gmail.com>
2018-11-17 03:08:44 +0500

    first login demo

---
+++ views/loginLayout/login.php
@@ -0,0 +1,31 @@
+<section id="login">
+	<div class="container align-items-center">
+		<div class="row  justify-content-center  vertical_center">
+			<div class="col-md-6 col-sm-12 col-xs-12 col-lg-6 ">
+				<div class="card align-items-center  justify-content-center" >
+  					<div class="card-body">
+    					<h2 class="card-title justify-content-center align-items-center">Phd Scholars</h2>
+    					<form>
+  							<div class="form-group">
+    							
+    							<input type="text" class="form-control"  placeholder="Name">
+   								 
+  							</div>
+  							<div class="form-group">
+   								 
+    							<input type="password" class="form-control"  placeholder="Password">
+  							</div>
+  							
+  							<div class="text-right col-md-12  ">
+  								<div class="col-md-ofset-4">
+  									<button type="submit" class="btn btn-success submit ">Login</button>
+  								</div>
+  									
+  							</div>
+  						</form>
+  					</div>
+  				</div>
+  			</div>
+		</div>
+	</div>
+</section>
---
+++ views/masterlayout.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Phd Scholars| Portal</title>
+	<link rel="stylesheet" type="text/css" href="../public/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="../public/css/login.css">
+</head>
+<body>
+	
+	<?php
+		include("loginLayout/login.php"); 
+	?>
+
+
+
+
+
+
+	<script type="text/javascript" src="../public/js/jquery.min.js"></script>
+	<script type="text/javascript" src="../public/js/popper.min.js"></script>
+	<script type="text/javascript" src="../public/js/bootstrap.min.js"></script>
+
+</body>
+</html>
